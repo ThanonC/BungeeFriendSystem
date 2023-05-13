1a3b2ec9673229381f43a404b9613962f5e04469
ThanonC <newthanonmail@gmail.com>
2023-05-13 22:40:26 +0200

    Initial commit

---
+++ src/main/java/net/thanon/friendsystem/FriendSystem.java
@@ -0,0 +1,16 @@
+package net.thanon.friendsystem;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class FriendSystem extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
